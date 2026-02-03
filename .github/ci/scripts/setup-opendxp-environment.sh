28b2c4c4d157e955db8802c36e9a1cd96c26d186
open-dxp-stack <stack@opendxp.ch>
2026-02-03 10:52:12 +0100

    Update setup-opendxp-environment.sh

--- .github/ci/scripts/setup-opendxp-environment.sh
+++ .github/ci/scripts/setup-opendxp-environment.sh
@@ -2,14 +2,11 @@
 
 set -eu
 
-mkdir -p var/config
-mkdir -p bin
-
-cp -r .github/ci/files/config/. config
-cp -r .github/ci/files/templates/. templates
-cp -r .github/ci/files/bin/console bin/console
-cp -r .github/ci/files/kernel/. kernel
-cp -r .github/ci/files/public/. public
+cp -r .github/ci/files/config config
+cp -r .github/ci/files/templates templates
+cp -r .github/ci/files/bin bin
+cp -r .github/ci/files/kernel kernel
+cp -r .github/ci/files/public public
 cp -r .github/ci/files/var var
 cp .github/ci/files/.env ./
 
