a1c90a1325e58fdefcee46bf38eff23d0ff9a022
open-dxp-stack <stack@opendxp.ch>
2026-02-03 10:22:55 +0100

    update test files

--- .github/ci/scripts/setup-opendxp-environment.sh
+++ .github/ci/scripts/setup-opendxp-environment.sh
@@ -4,35 +4,13 @@ set -eu
 
 mkdir -p var/config
 mkdir -p bin
-mkdir -p kernel
 
 cp -r .github/ci/files/config/. config
 cp -r .github/ci/files/templates/. templates
 cp -r .github/ci/files/bin/console bin/console
+cp -r .github/ci/files/kernel/. kernel
 cp -r .github/ci/files/public/. public
+cp .github/ci/files/.env ./
 
 chmod 755 bin/console
 
-#
-# Codeception Framework Stuff
-
-touch ./.env
-
-TEST_BUNDLE_TEST_DIR="$OPENDXP_PROJECT_ROOT/tests"
-OPENDXP_CODECEPTION_FRAMEWORK="${OPENDXP_CODECEPTION_FRAMEWORK_PATH:-$OPENDXP_PROJECT_ROOT/opendxp-codeception-framework}"
-
-cp "$OPENDXP_CODECEPTION_FRAMEWORK/src/Support/App/TestKernel.php" kernel/TestKernel.php
-
-#
-## Register test variables in .env
-{
-  echo "TEST_BUNDLE_NAME=OpenDxpEcommerceFrameworkBundle"
-  echo "TEST_BUNDLE_NAMESPACE=OpenDxp\Bundle\EcommerceFrameworkBundle"
-  echo "TEST_BUNDLE_INSTALLER_CLASS=OpenDxp\Bundle\EcommerceFrameworkBundle\Tools\Installer"
-
-  echo "APP_ENV=test"
-  echo "APP_DEBUG=true"
-  echo "OPENDXP_KERNEL_CLASS=\App\TestKernel"
-  echo "OPENDXP_CODECEPTION_FRAMEWORK=$OPENDXP_CODECEPTION_FRAMEWORK"
-  echo "TEST_BUNDLE_TEST_DIR=$TEST_BUNDLE_TEST_DIR"
-} >> ./.env
