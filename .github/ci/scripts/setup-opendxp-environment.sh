aff26a359c041eda1df9af9568262eda13948e28
open-dxp-stack <stack@opendxp.ch>
2026-02-03 10:34:51 +0100

    update test files

--- .github/ci/scripts/setup-opendxp-environment.sh
+++ .github/ci/scripts/setup-opendxp-environment.sh
@@ -10,6 +10,7 @@ cp -r .github/ci/files/templates/. templates
 cp -r .github/ci/files/bin/console bin/console
 cp -r .github/ci/files/kernel/. kernel
 cp -r .github/ci/files/public/. public
+cp -r .github/ci/files/var var
 cp .github/ci/files/.env ./
 
 chmod 755 bin/console
